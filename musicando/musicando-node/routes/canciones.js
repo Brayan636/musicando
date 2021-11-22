09e98e5cb713de121a086f0da6886a806c0a4078
Brayan63 <brayan636garcia@gmail.com>
2021-11-22 14:58:02 -0600

    sdfsd

--- musicando/musicando-node/routes/canciones.js
+++ musicando/musicando-node/routes/canciones.js
@@ -1,14 +1,6 @@
-const { Router } = require('express');
-var express = require('express');
-var router = express.Router();
-var cancionesController = require("../controllers/cancionesController")
-//creacion
+const express = require('express');
+const router = express.Router();
+const controller = require("../controllers/cancionesController")
 
-router.get("/crear", cancionesController.crear);
-router.post("/crear", cancionesController.guardado);
-router.get("/",cancionesController.listado );
-router.get("/:id", cancionesController.detalle);
-router.get ("/editar/:id", cancionesController.editar)
-router.put ("/editar/:id", cancionesController.actualizar)
-router.delete ("/borrar", cancionesController.borrar)
+router.get("/", controller.list)
 module.exports = router;
