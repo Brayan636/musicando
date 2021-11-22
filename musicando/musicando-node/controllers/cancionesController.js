ef85cec63406b48824e6d525d0327657e8541ac0
Brayan63 <brayan636garcia@gmail.com>
2021-11-22 02:08:47 -0600

    sdsdfsd

--- musicando/musicando-node/controllers/cancionesController.js
+++ musicando/musicando-node/controllers/cancionesController.js
@@ -6,7 +6,59 @@ let cancionesController = {
      .then(function(generos){
          return res.render("listadoCanciones", {generos:generos});
      })
+    },
+    guardado:  function(req, res) {
+        db.Cancion.create({
+            titulo: req.body.titulo,
+            album: req.body.album,
+            artista: req.body.artista,
+            duracion: req.body.duracion,
+            genero: req.body.genero
+        });
+        res.redirect("/canciones")
+    },
+    listado: function (req, res) {
+        db.Cancion.findAll()
+    
+     .then(function(canciones){
+         res.render("listadoCanciones", {canciones:canciones});
+     })
+    },
+     detalle: function(req, res){
+         db.cancion.findByPk(req.params.id, {
+             include: [{association: "genero"}, {association:"artistas"}]
+         })
+         .then(function(cancion){
+             res.render("detalleCancion", {cancion:cancion});
+         })
+        },
+         editar: function(req, res){
+            let pedidoCancion = db.Cancion.findByPk(req.params.id)
+            let pedidoGeneros = db.Genero.findAll();
+            Promise.all([pedidoCancion, pedidoGeneros])
+            .then(function([cancion, generos]){
+              res.render("editarCancion", {cancion:cancion, generos: generos})
+            })
+            },
+            actualizar: function(req, res){
+                  db.Cancion.update({
+                    titulo: req.body.titulo,
+                    album: req.body.album,
+                    artista: req.body.artista,
+                    duracion: req.body.duracion,
+                    genero: req.body.genero
+                }, {where:{
+                    id: req.params.id
+                }});
+                res.redirect("/canciones/" + req.params.id)
+            },
+            borrar: function(req, res){
+                db.Cancion.destroy({
+                  where:{
+                  id: req.params.id
+              }});
+              res.redirect("/canciones")
+          },
     }
-}
-
+            
 module.exports = cancionesController;
--- musicando/musicando-node/routes/canciones.js
+++ musicando/musicando-node/routes/canciones.js
@@ -1,8 +1,14 @@
+const { Router } = require('express');
 var express = require('express');
 var router = express.Router();
 var cancionesController = require("../controllers/cancionesController")
 //creacion
 
 router.get("/crear", cancionesController.crear);
-
+router.post("/crear", cancionesController.guardado);
+router.get("/",cancionesController.listado );
+router.get("/:id", cancionesController.detalle);
+router.get ("/editar/:id", cancionesController.editar)
+router.put ("/editar/:id", cancionesController.actualizar)
+router.delete ("/borrar", cancionesController.borrar)
 module.exports = router;
