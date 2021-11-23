b5e1dfe974645dcfc3762730eb03b82526a9fc74
Brayan63 <brayan636garcia@gmail.com>
2021-11-23 00:32:07 -0600

    end points

--- musicando/musicando-node/controllers/cancionesController.js
+++ musicando/musicando-node/controllers/cancionesController.js
@@ -5,7 +5,73 @@ module.exports = {
     list:(req, res) => {
         DB.Cancion.findAll()
         .then(canciones => {
-            return res.json (canciones)
+            return res.status(200).json ({
+                total: canciones.length,
+                data:  canciones,
+                status: 200
+            })
         })
+    },
+    mostrar:(req, res) => {
+        DB.Cancion.findByPk(req.params.id)
+        .then(cancion => {
+            return res.status(200).json ({
+                data:  cancion,
+                status: 200
+})
+})
+},
+crear:(req, res) => {
+    DB.Cancion.create(req.body)
+    .then(cancion => {
+        return res.status(200).json ({
+            data:  cancion,
+            status: 200,
+            created: "Listo"
+})
+})
+},
+borrar:(req, res) => {
+    DB.Cancion.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(response => {
+        return res.json(response) 
+    })
+},
+generos:(req, res) => {
+    DB.Cancion.findAll()
+    .then(canciones, generos => {
+        return res.status(200).json ({
+            data:  canciones, generos,
+            status: 200
+})
+    })
+},
+  editar:(req, res) => {
+    DB.Cancion.findByPk(req.params.id)
+    .then(function(cancion)  {
+        return res.status(200).json ({
+            data:  cancion, 
+            status: 200
+})
+    })
+},
+update: (req, res)=>{
+    DB.Cancion.update({
+    titulo: req.body.titulo,
+    duracion: req.body.duracion,
+    genero:  req.body.genero,
+    album:  req.body.album,
+    artista:  req.body.artista
+           
+    },{ 
+    where:{
+        id: req.params.id
     }
+})
+
+}
 }
--- musicando/musicando-node/routes/canciones.js
+++ musicando/musicando-node/routes/canciones.js
@@ -2,5 +2,13 @@ const express = require('express');
 const router = express.Router();
 const controller = require("../controllers/cancionesController")
 
-router.get("/", controller.list)
+router.get("/", controller.list);
+router.post("/", controller.crear );
+router.get("/:id", controller.mostrar);
+router.put("/:id", controller.editar);
+router.post("/:id", controller.update);
+router.delete("/:id", controller.borrar)
+router.get("/generos", controller.generos);
+
+
 module.exports = router;
